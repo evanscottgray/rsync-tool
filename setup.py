d232e8875b2b4f9b823db63f8bfaeaa225cf02ef
Kevin George <kevingeorgetm@gmail.com>
2014-09-22 03:27:45 +0000

    Python project structure

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+#   {one line to give the program's name and a brief idea of what it does.}
+#   Copyright (C) 2014  Kevin George
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import setuptools
+
+
+requirements = [
+    ]
+
+
+setuptools.setup(
+    name="rsync-tool",
+    version="0.2",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    author="Kevin George",
+    author_email="kevingeorgetm@gmail.com",
+    )
+
